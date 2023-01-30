d6b4d029ffffad8949ae946d363705f23c00b93a
Anton Schirg <anton.schirg@posteo.de>
2023-01-30 23:28:24 +0100

    overlay is not necessary

--- package.nix
+++ package.nix
@@ -3,7 +3,11 @@ stdenv.mkDerivation {
   name = "cmake_cross_test";
   src = ./.;
   nativeBuildInputs = [ cmake ];
+  
+  # Graphvis has to be in build build or it doesn't work. I don't fully understand.
+  # I thought since it isn't a compiler nativeBuildInputs of depsBuildBuild doesn't matter.
   depsBuildBuild = [ graphviz ];
+
   buildInputs = [
     (boost.override {
         specificLibs = [ "system" "filesystem" "serialization" ];
--- windows.nix
+++ windows.nix
@@ -1,10 +1,1 @@
-let
-  overlay = (self: super: {
-      cmake_cross_test = super.callPackage ./package.nix { };
-   });
-  winpkgs = import <nixpkgs> {
-    crossSystem = (import <nixpkgs>{}).lib.systems.examples.mingwW64;
-    overlays = [ overlay ];
-  };
- 
-in winpkgs.cmake_cross_test
+(import <nixpkgs>{crossSystem = (import <nixpkgs>{}).lib.systems.examples.mingwW64;}).callPackage ./package.nix { }
